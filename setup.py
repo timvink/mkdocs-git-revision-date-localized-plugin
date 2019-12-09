03016f22300d409d372dd508503bff8e468c67d5
Tim Vink <vinktim@gmail.com>
2019-12-09 13:46:49 +0100

    fix pypi installation

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='mkdocs-git-revision-date-localized-plugin',
-    version='0.2',
+    version='0.2.1',
     description='Mkdocs plugin that enables displaying the localized date of the last git modification of a markdown file.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords='mkdocs git date timeago babel plugin',
-    url='https://github.com/timvink/mkdocs-git-revision-date-localized-plugin/',
+    url='https://github.com/timvink/mkdocs-git-revision-date-localized-plugin',
     author='Tim Vink',
     author_email='vinktim@gmail.com',
     license='MIT',
     python_requires='>=3.4',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'mkdocs>=0.17',
         'GitPython',
