6ca13cb9391a56553480e45dc2370f1cdd70bdc7
Tim Vink <vinktim@gmail.com>
2020-02-22 21:01:05 +0100

    Throw usefull error messages when using CI runners, see #10

--- README.md
+++ README.md
@@ -30,6 +30,13 @@ plugins:
 
 > If you have no `plugins` entry in your config file yet, you'll likely also want to add the `search` plugin. MkDocs enables it by default if there is no `plugins` entry set.
 
+### When using CI runners
+
+The plugin needs access to the last commit that touched a file to be able to retrieve the date. If you build your docs using CI then you might need to change the default settings: 
+
+- github actions: set `fetch_depth` to `0` ([docs](https://github.com/actions/checkout))
+- gitlab runners: set `GIT_DEPTH` to `1000` ([docs](https://docs.gitlab.com/ee/user/project/pipelines/settings.html#git-shallow-clone))
+
 ## Usage
 
 ### In supported themes
--- mkdocs_git_revision_date_localized_plugin/util.py
+++ mkdocs_git_revision_date_localized_plugin/util.py
@@ -1,3 +1,5 @@
+import os
+import logging
 from git import Git
 from datetime import datetime
 from babel.dates import format_date
@@ -7,7 +9,35 @@ class Util:
     def __init__(self):
         self.g = Git()
 
-    def get_revision_date_for_file(self, path: str, locale: str = 'en'):
+    @staticmethod
+    def _date_formats(unix_timestamp, locale = 'en'):
+        """
+        Returns different date formats / types.
+        
+        Args:
+            unix_timestamp (datetiment): a timestamp in seconds since 1970
+            locale (str): Locale code of language to use. Defaults to 'en'.
+            
+        Returns:
+            dict: different date formats
+        """
+        
+        # Convert to millisecond timestamp
+        unix_timestamp = int(unix_timestamp)
+        timestamp_in_ms = unix_timestamp * 1000
+        
+        revision_date = datetime.utcfromtimestamp(unix_timestamp)
+        
+        return {
+            'date' : format_date(revision_date, format="long", locale=locale), 
+            'datetime' : format_date(revision_date, format="long", locale=locale) + ' ' +revision_date.strftime("%H:%M:%S"),
+            'iso_date' : revision_date.strftime("%Y-%m-%d"), 
+            'iso_datetime' : revision_date.strftime("%Y-%m-%d %H:%M:%S"), 
+            'timeago' : "<span class='timeago' datetime='%s' locale='%s'></span>" % (timestamp_in_ms, locale)
+        }
+        
+
+    def get_revision_date_for_file(self, path, locale = 'en'):
         """
         Determine localized date variants for a given file
         
@@ -21,19 +51,26 @@ class Util:
         
         unix_timestamp = self.g.log(path, n=1, date='short', format='%at')
         
+
         if not unix_timestamp:
-            unix_timestamp = datetime.utcnow().timestamp() 
-            print('WARNING - %s has no git logs, using current timestamp' % path)
+            
+            if os.environ.get('GITLAB_CI'):
+                raise EnvironmentError("""Cannot access git commit for '%s'. 
+                       Try setting GIT_DEPTH to 1000 in your .gitlab-ci.yml file.
+                       (see https://docs.gitlab.com/ee/user/project/pipelines/settings.html#git-shallow-clone).
+                       Or disable mkdocs-git-revision-date-localized-plugin when using gitlab runners.
+                       """ % path)
+
+            if os.environ.get('GITHUB_ACTIONS'):
+                raise EnvironmentError("""Cannot access git commit for '%s'. 
+                       Try setting fetch-depth to 1 in your github action
+                       (see https://github.com/actions/checkout).
+                       Or disable mkdocs-git-revision-date-localized-plugin when using github actions.
+                       """ % path)
+
+            unix_timestamp = datetime.utcnow().timestamp()
+            logging.warning('%s has no git logs, using current timestamp' % path)
+            
+
         
-        unix_timestamp = int(unix_timestamp)
-        timestamp_in_ms = unix_timestamp * 1000
-        
-        revision_date = datetime.utcfromtimestamp(unix_timestamp)
-        
-        return {
-            'date' : format_date(revision_date, format="long", locale=locale), 
-            'datetime' : format_date(revision_date, format="long", locale=locale) + ' ' +revision_date.strftime("%H:%M:%S"),
-            'iso_date' : revision_date.strftime("%Y-%m-%d"), 
-            'iso_datetime' : revision_date.strftime("%Y-%m-%d %H:%M:%S"), 
-            'timeago' : "<span class='timeago' datetime='%s' locale='%s'></span>" % (timestamp_in_ms, locale)
-        }
+        return self._date_formats(unix_timestamp)
