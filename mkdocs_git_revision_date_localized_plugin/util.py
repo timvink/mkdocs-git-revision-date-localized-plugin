af27290b2d88a309903b786e60c0aba0e7cae674
Tim Vink <vinktim@gmail.com>
2024-02-03 08:05:41 +0000

    Revert "For jinja autoescape, mark HTML strings as safe, not needing escaping"
    This reverts commit 77bff0c760ff6906b83d3908028621b8cf3da8b0.

--- mkdocs_git_revision_date_localized_plugin/util.py
+++ mkdocs_git_revision_date_localized_plugin/util.py
@@ -3,8 +3,6 @@ import logging
 import os
 import time
 
-from markupsafe import Markup
-
 from mkdocs_git_revision_date_localized_plugin.ci import raise_ci_warnings
 from mkdocs_git_revision_date_localized_plugin.dates import get_date_formats
 
@@ -87,7 +85,7 @@ class Util:
                 commit_timestamp = git.log(
                     realpath, date="unix", format="%at", diff_filter="A", no_show_signature=True, follow=True
                 )
-                # A file can be created multiple times, through a file renamed.
+                # A file can be created multiple times, through a file renamed. 
                 # Commits are ordered with most recent commit first
                 # Get the oldest commit only
                 if commit_timestamp != "":
@@ -167,7 +165,7 @@ class Util:
             dict: Localized date variants.
         """
         date_formats = get_date_formats(
-            unix_timestamp=commit_timestamp,
+            unix_timestamp=commit_timestamp, 
             time_zone=self.config.get("timezone"),
             locale=locale,
             custom_format=self.config.get('custom_format')
@@ -185,7 +183,7 @@ class Util:
         """
         for date_type, date_string in date_formats.items():
             date_formats[date_type] = (
-                Markup('<span class="git-revision-date-localized-plugin git-revision-date-localized-plugin-%s">%s</span>')
+                '<span class="git-revision-date-localized-plugin git-revision-date-localized-plugin-%s">%s</span>'
                 % (date_type, date_string)
             )
         return date_formats
